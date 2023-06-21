c739ec5332ae552789abb750db67d5cdb9290f18
heartblood91 <pro.cedric@proton.me>
2023-06-21 17:53:13 +0200

    clean git history

---
+++ bin/config/development.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# set -x
+
+export BLOOMAYS_APP_URL='https://localhost'
+export BLOOMAYS_APP_PORT='3000'
+
+export BLOOMAYS_API_PORT='4000'
+
+export BLOOMAYS_AIRTABLE_TOKEN=''
+export BLOOMAYS_AIRTABLE_BASE_ID=''
